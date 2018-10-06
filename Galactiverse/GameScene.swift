d4950577c8abf5bcc15653321e58c8158d4505a3
Jake Garcia <bballwiz4@gmail.com>
2018-10-06 09:31:04 -0700

    setup alien and player ship. make aliens appear in a certain amount of rows and columns and a certain distance away from each other

--- Galactiverse/GameScene.swift
+++ Galactiverse/GameScene.swift
@@ -10,28 +10,51 @@ import SpriteKit
 import GameplayKit
 import CoreMotion
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    enum InvaderType {
+        case a
+        case b
+        case c
+        static var size: CGSize {
+            return CGSize(width: 24, height: 16)
+        }
+        
+        static var name: String {
+            return "invader"
+        }
+    }
+    
+    let kInvaderGridSpacing = CGSize(width: 20, height: 20)
+    let kInvaderRowCount = 9
+    let kInvaderColCount = 9
+    let kShipSize = CGSize(width: 40, height: 24)
+    let kShipName = "ship"
+    
+    
     
     var player: SKSpriteNode!
     var lastTouchPosition: CGPoint?
     var motionManager: CMMotionManager!
-    
-    enum CollisionTypes: UInt32 {
-        case player = 1
-        case wall = 2
-        case star = 4
-        case vortex = 8
-        case finish = 16
+    var starField:SKEmitterNode!
+    var scoreLabel:SKLabelNode!
+    var score:Int = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
     }
     
     override func didMove(to view: SKView) {
-        
-        createPlayer()
-        
+        self.anchorPoint = CGPoint(x: 0, y: 0)
+        createBackground()
+        // createPlayer()
+        setupShip()
+        placeScoreLabel()
         physicsWorld.gravity = CGVector(dx: 0, dy: 0)
-        
+        physicsWorld.contactDelegate = self
         motionManager = CMMotionManager()
         motionManager.startAccelerometerUpdates()
+        setupInvader()
         
     }
     
@@ -57,36 +80,103 @@ class GameScene: SKScene {
         lastTouchPosition = nil
     }
     // Create the player, node, and collision bit mask
-    func createPlayer() {
-        player = SKSpriteNode(imageNamed: "ship")
-        player.position = CGPoint(x: 0, y: 0)
-        player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width / 2)
-        player.physicsBody?.allowsRotation = false
-        player.physicsBody?.linearDamping = 0.5
+//    func createPlayer() {
+//        player = SKSpriteNode(imageNamed: "ship")
+//        player.position = CGPoint(x: frame.size.width/2, y: player.size.height/2 + 20)
+//        player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width / 2)
+//        player.physicsBody?.allowsRotation = false
+//        player.physicsBody?.linearDamping = 0.5
+//
+//        addChild(player)
+//    }
+    // Create the background by using an SKEmitterNode
+    func createBackground() {
         
-        player.physicsBody?.categoryBitMask = CollisionTypes.player.rawValue
-        player.physicsBody?.contactTestBitMask = CollisionTypes.star.rawValue | CollisionTypes.vortex.rawValue | CollisionTypes.finish.rawValue
-        player.physicsBody?.collisionBitMask = CollisionTypes.wall.rawValue
-        addChild(player)
+        starField = SKEmitterNode(fileNamed: "starfield")
+        starField.position = CGPoint(x: frame.size.width/2, y: frame.size.height)
+        starField.advanceSimulationTime(10)
+        starField.zPosition = -1
+        self.addChild(starField)
+    }
+    // Place the score label in the top left corner of any screen
+    func placeScoreLabel() {
+        scoreLabel = SKLabelNode(text: "Score: 0")
+        scoreLabel.position = CGPoint(x: self.frame.size.width/10, y: self.frame.size.height - 60)
+        scoreLabel.horizontalAlignmentMode = .left
+        scoreLabel.verticalAlignmentMode = .top
+        scoreLabel.fontName = "sans-serif"
+        scoreLabel.fontSize = 36
+        scoreLabel.fontColor = UIColor.white
+        score = 0
+        self.addChild(scoreLabel)
     }
     
+    //Add enemy aliens
+    func addAlien(ofType invaderType: InvaderType) -> SKNode {
+        var invaderColor: SKColor
+        switch(invaderType) {
+        case .a:
+            invaderColor = SKColor.red
+        case .b:
+            invaderColor = SKColor.green
+        case .c:
+            invaderColor = SKColor.blue
+        }
+        let invader = SKSpriteNode(color: invaderColor, size: InvaderType.size)
+        invader.name = InvaderType.name
+        return invader
+    }
     
-   
+    func setupInvader() {
+        let baseOrigin = CGPoint(x: size.width / 3, y: size.height / 2)
+        
+        for row in 0..<kInvaderRowCount {
+            var invaderType: InvaderType
+            if row % 3 == 0 {
+                invaderType = .a
+            } else if row % 3 == 1 {
+                invaderType = .b
+            } else {
+                invaderType = .c
+            }
+            
+            let invaderPositionY = CGFloat(row) * (InvaderType.size.height * 2) + baseOrigin.y
+            var invaderPosition = CGPoint(x: baseOrigin.x, y: invaderPositionY)
+            
+            for _ in 1..<kInvaderColCount {
+                let invader = addAlien(ofType: invaderType)
+                invader.position = invaderPosition
+                
+                addChild(invader)
+                
+                invaderPosition = CGPoint(
+                    x: invaderPosition.x + InvaderType.size.width + kInvaderGridSpacing.width,
+                    y: invaderPositionY
+                )
+            }
+        }
+    }
+    
+    // Create and setuo ship
+    func setupShip() {
+        let ship = makeShip()
+        
+        ship.position = CGPoint(x: size.width / 2.0, y: (kShipSize.height / 2.0) + 20)
+        addChild(ship)
+    }
+    
+    func makeShip() -> SKNode {
+        let ship = SKSpriteNode(color: SKColor.green, size: kShipSize)
+        ship.name = kShipName
+        return ship
+    }
     
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
-        //what is happening
-        //Checks if the game is being run on the simulator
-        #if targetEnvironment(simulator)
-            if let currentTouch = lastTouchPosition {
-                let diff = CGPoint(x: currentTouch.x - player.position.x, y: currentTouch.y - player.position.y)
-                physicsWorld.gravity = CGVector(dx: diff.x/1000, dy: diff.y/1000)
-            }
-        #else
         if let accelerometerData = motionManager.accelerometerData {
-            physicsWorld.gravity = CGVector(dx: accelerometerData.acceleration.x * 10, dy: accelerometerData.acceleration.y * 10)
+            physicsWorld.gravity = CGVector(dx: accelerometerData.acceleration.x * 15, dy: 0)
         }
-        #endif
     }
+    
 }
